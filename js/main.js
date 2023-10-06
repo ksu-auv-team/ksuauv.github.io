f4b74e52961fd61590aba3d73fd6251e0dbcf79f
surgosan <sergy200118@gmail.com>
2023-10-06 18:01:27 -0400

    Documentation Links Linked

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const currentPageUrl = window.location.pathname;
-    const navLinks = document.querySelectorAll("nav a");
+    // Check if the current window is the top-level window
 
-    navLinks.forEach((link) => {
-        const linkHref = link.getAttribute("href");
-        if (currentPageUrl.endsWith(linkHref)) {
-            link.classList.add("active");
-        }
-    });
+        // This code will run only in the top-level window
+        const currentPageUrl = window.parent.location.pathname;
+        const currentPageEnd = currentPageUrl.split('/').pop();
+        const navLinks = document.querySelectorAll("nav a");
+        const documentationPages = ["2020_sub_kirby.html"];
+
+        navLinks.forEach((link) => {
+
+            const linkHref = link.getAttribute("href");
+            // alert(currentPageEnd + " = " + documentationPages[0] + "\nlinkhref: " + linkHref + "\nCurrentButton: " + link)
+
+            if (currentPageUrl === linkHref) {
+                // link.classList.add("active");
+                document.getElementById('doc_link').classList.add('active');
+            }
+
+            if(documentationPages.includes(currentPageEnd)) {
+                document.getElementById('doc_link').classList.add('go');
+            }
+        });
 });
+
+
 
 function openDialog() {
     window.parent.document.getElementById('dialog').style.display = 'initial';
